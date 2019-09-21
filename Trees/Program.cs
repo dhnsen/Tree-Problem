14f5672880c60f641bf99e66cc5e220791a72071
Dan H <spag1024@hotmail.com>
2019-09-21 18:16:38 -0500

    tested solution. everything passed

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -12,6 +12,9 @@ namespace Trees
         {
             Console.WriteLine("Harvest Two Sections:\n");
             Console.WriteLine("Expect 80: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 0, 0, 20, 20, 20 }, 2, 3));
+            Console.WriteLine("Expect 90: " + Harvest.HarvestTwoSections(new int[] { 20, 10, 0, 0, 20, 20, 20 }, 2, 3));
+            Console.WriteLine("Expect 60: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 0, 0, 20, 20, 0 }, 2, 3));
+            Console.WriteLine("Expect 85: " + Harvest.HarvestTwoSections(new int[] { 10, 15, 0, 0, 20, 20, 20 }, 2, 3));
             Console.WriteLine("Expect 90: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 20, 20, 20 }, 3, 2));
             Console.WriteLine("Expect 100: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 30, 20, 20 }, 2, 3));
             Console.WriteLine("Expect 100: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 20, 20, 20, 0, 20, 20 }, 2, 3));
