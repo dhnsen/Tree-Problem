ffb3f5ed13768f95b244b18f34ae8ef3e358640c
Dan H <spag1024@hotmail.com>
2019-09-21 15:35:58 -0500

    realized I missed an edge case

--- Trees/Program.cs
+++ Trees/Program.cs
@@ -11,10 +11,10 @@ namespace Trees
         static void Main(string[] args)
         {
             Console.WriteLine("Harvest Two Sections:\n");
-            Console.WriteLine("Expect 80: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 0, 0, 20, 20, 20 }, 3, 2));
+            Console.WriteLine("Expect 80: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 0, 0, 20, 20, 20 }, 2, 3));
             Console.WriteLine("Expect 90: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 20, 20, 20 }, 3, 2));
-            Console.WriteLine("Expect 100: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 30, 20, 20 }, 3, 2));
-            Console.WriteLine("Expect 100: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 20, 20, 20, 0, 20, 20 }, 3, 2));
+            Console.WriteLine("Expect 100: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 30, 20, 20 }, 2, 3));
+            Console.WriteLine("Expect 100: " + Harvest.HarvestTwoSections(new int[] { 10, 10, 20, 0, 0, 20, 20, 20, 0, 20, 20 }, 2, 3));
 
             Console.ReadLine();
         }
